6aafdcbfc14c47cfe41f993253b9d5f41e5d6ecd
Jordan Walker <jordanswalker28@gmail.com>
2021-03-13 16:45:08 +0000

    Added file reader and data

--- Applications/HigherOrLower/Program.cs
+++ Applications/HigherOrLower/Program.cs
@@ -6,7 +6,69 @@ namespace HigherOrLower
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            playGame();
+            Console.WriteLine("Thank you for playing");
         }
+
+        private static void playGame()
+        {
+            bool flag = true;
+
+            while (flag)
+            {
+                Random rnd = new Random();
+                int gameNumber = rnd.Next(1, 100);
+                mainGameLoop(gameNumber);
+                Console.WriteLine("Do you want to play again? y/n");
+                flag = continueGame();
+                if (flag)
+                {
+                    continue;
+                }
+                else
+                {
+                    break;
+                }
+            }
+        }
+
+        private static bool continueGame()
+        {
+            string choice = Console.ReadLine();
+            bool result = true;
+            choice = choice.ToLower();
+            Console.WriteLine("Speak priest!");
+            if (choice == "y")
+            {
+                return true;
+            }
+            else if (choice == "n")
+            {
+                return false;
+            }
+            else
+            {
+                Console.WriteLine("only values y/n apply here");
+                continueGame();
+            }
+         
+
+            return result;
+        }
+
+        private static void mainGameLoop(int gameNumber)
+        {
+            Console.WriteLine(gameNumber);
+            int playerChoice = -1;
+            int computerChoice = -1;
+            while (playerChoice != gameNumber || computerChoice != gameNumber)
+            {
+                string guess = Console.ReadLine();
+                playerChoice = Convert.ToInt32(guess);
+                Console.WriteLine(playerChoice);
+
+            }
+        }
+
     }
 }
--- Applications/NameGenerator/Program.cs
+++ Applications/NameGenerator/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.IO;
 
 namespace NameGenerator
 {
@@ -6,7 +8,23 @@ namespace NameGenerator
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello Random Name!");
+            string filePath = "/Users/jordanswalker/Documents/99ConsoleApplications/99ConsoleApplications/Applications/NameGenerator/len-us-first-names-database/babynames-clean.csv";
+            using (var reader = new StreamReader(@filePath))
+            {
+                Dictionary<string, string> nameGender = new Dictionary<string, string>();
+
+                while (!reader.EndOfStream)
+                {
+                    var line = reader.ReadLine();
+                    var values = line.Split(',');
+                    nameGender.Add(values[0], values[1]);
+                }
+
+                foreach (var item in nameGender)
+                {
+                    Console.WriteLine(item);
+                }
+            }
         }
     }
 }
